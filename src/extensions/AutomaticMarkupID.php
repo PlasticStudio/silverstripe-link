1433e5777aa4d6aafe3f26c22c32b997bae13348
ebakernz <emma@psdigital.co.nz>
2025-11-17 10:39:11 +1300

    update

--- src/extensions/AutomaticMarkupID.php
+++ src/extensions/AutomaticMarkupID.php
@@ -3,7 +3,7 @@
 namespace gorriecoe\Link\Extensions;
 
 use SilverStripe\Core\Convert;
-use SilverStripe\ORCoreM\Extension;
+use SilverStripe\Core\Extension;
 
 /**
  * Add sitetree type to link field
